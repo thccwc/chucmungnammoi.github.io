d25009c024f55a7c7acfe646fad7b09cb6cc6863
thccwc <phamh239@newschool.edu>
2025-02-18 18:44:40 -0500

    yay

--- entry-3/script.js
+++ entry-3/script.js
@@ -1,13 +1,28 @@
-let anchorlinks = document.querySelectorAll('a[href^="#"]')
+let anchorlinks = document.querySelectorAll('a[href^="#"]');
 
-for (let item of anchorlinks) { // relitere 
-    item.addEventListener('click', (e)=> {
-		let hashval = item.getAttribute('href')
-		let target = document.querySelector(hashval)
-		target.scrollIntoView({
-			behavior: 'smooth'
-		})
-		history.pushState(null, null, hashval)
-		e.preventDefault()
-	})
+for (let item of anchorlinks) {
+    item.addEventListener('click', (e) => {
+        e.preventDefault();
+        
+        let hashval = item.getAttribute('href');
+        let target = document.querySelector(hashval);
+
+        if (!target) return;
+
+        let scrollContainer = target.closest('.content') || document.documentElement;
+
+        scrollContainer.scrollTo({
+            top: target.offsetTop - scrollContainer.offsetTop,
+            behavior: 'smooth'
+        });
+
+        history.pushState(null, null, hashval);
+    });
+	document.querySelector('.content').querySelector('#sectionc').scrollIntoView({
+		behavior: 'smooth',
+		block: 'start'
+	});
+	
 }
+
+
